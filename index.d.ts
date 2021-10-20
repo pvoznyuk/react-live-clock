f18e8eabca1dceb55d7c2efa6cca52bf7ac4d256
abourque <abourque@spiria.com>
2021-10-20 08:56:10 -0400

    Update TypeScript Declaration file to include 'className', 'locale' and 'style' and sort declarations

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,17 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 interface Props {
   readonly children?: string;
+  readonly className?: string;
   readonly date?: number | string;
+  readonly filter?: () => void;
   readonly format?: string;
   readonly interval?: number;
+  readonly locale?: string;
+  readonly onChange?: () => void;
+  readonly style?: CSSProperties;
   readonly ticking?: boolean;
   readonly timezone?: string;
-  readonly filter?: () => void;
-  readonly onChange?: () => void;
 }
 
 declare class SimpleSelect extends Component<Props, any> {}

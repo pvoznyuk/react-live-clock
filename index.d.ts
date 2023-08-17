bc4756d0938f9394db0a294bb63ceacd6813aff3
Manoj Sethi <manojsethi@live.in>
2023-08-17 13:15:49 +0530

    now is a number

--- index.d.ts
+++ index.d.ts
@@ -9,7 +9,7 @@ interface Props {
   readonly format?: string;
   readonly interval?: number;
   readonly locale?: string;
-  readonly onChange?: (date: Date) => void;
+  readonly onChange?: (date: number) => void;
   readonly onReady?: () => void;
   readonly style?: React.CSSProperties;
   readonly ticking?: boolean;
